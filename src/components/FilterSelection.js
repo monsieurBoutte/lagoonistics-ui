11de0341614966cf5be9e09fbb39773bfeba7d2c
mrboutte101 <mrboutte21@gmail.com>
2018-11-03 22:34:00 -0400

    filtersection component

---
+++ src/components/FilterSelection.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import Chip from '@material-ui/core/Chip';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  formControl: {
+    margin: theme.spacing.unit,
+    minWidth: 120,
+    maxWidth: 300,
+  },
+  chips: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  chip: {
+    margin: theme.spacing.unit / 4,
+  },
+  noLabel: {
+    marginTop: theme.spacing.unit * 3,
+  },
+});
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 270,
+    },
+  },
+};
+
+const dataTypes = [
+  'Dissolved Oxygen',
+  'PH',
+  'Salinty',
+  'Chlorophyll',
+  'Turbidity (NTU)',
+  'Phosphate (mg P/L)',
+  'Temperature (F)',
+  'Blue/Green Algae (ug/L)',
+  'Depth (ft)',
+  'Direction (º)',
+];
+
+const FilterSelection = props => {
+  const [dataTypeSelection, setDataTypeSelection] = useState([])
+  const { classes } = props;
+
+  const handleChange = event => setDataTypeSelection(event.target.value);
+
+  return (
+    <div className={classes.root}>
+      <FormControl className={classes.formControl}>
+        <InputLabel htmlFor="select-multiple-chip">
+          Filtered Data Type
+        </InputLabel>
+        <Select
+          multiple
+          value={dataTypeSelection}
+          onChange={e => handleChange(e)}
+          input={<Input id="select-multiple-chip" />}
+          renderValue={selected => (
+            <div className={classes.chips}>
+              {selected.map(value => (
+                <Chip key={value} label={value} className={classes.chip} />
+              ))}
+            </div>
+          )}
+          MenuProps={MenuProps}
+          style={{ width: '280px' }}
+        >
+          {dataTypes.map(name => (
+            <MenuItem key={name} value={name}>
+              {name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
+
+export default withStyles(styles, { withTheme: true })(FilterSelection);
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -24,54 +24,13 @@ const styles = theme => ({
     zIndex: '1400',
   },
   menuButton: {
-    marginLeft: 4,
-    marginRight: -18,
+    marginLeft: 'auto',
+    marginRight: -10,
   },
   title: {
     display: 'none',
     [theme.breakpoints.up('sm')]: {
       display: 'block',
-    },
-  },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing.unit,
-      width: 'auto',
-    },
-  },
-  searchIcon: {
-    width: theme.spacing.unit * 9,
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-    width: '100%',
-  },
-  inputInput: {
-    paddingTop: theme.spacing.unit,
-    paddingRight: theme.spacing.unit,
-    paddingBottom: theme.spacing.unit,
-    paddingLeft: theme.spacing.unit * 10,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: 120,
-      '&:focus': {
-        width: 200,
-      },
     },
   },
 });
@@ -87,19 +46,6 @@ const NavBar = props => {
           <Typography className={classes.title} variant="h6" color="inherit" noWrap>
             Lagoonistics
           </Typography>
-          <div className={classes.grow} />
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-            <InputBase
-              placeholder="Search…"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-            />
-          </div>
           <IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
             <SettingsIcon onClick={() => setIsOpen(!isOpen)} />
           </IconButton>
